4519f7b3fa5cfe11bf5bfb41bf8e3baf8a8f5747
mosh <sergey.moshkin92@gmail.com>
2021-06-08 23:52:45 +0300

    Initial commit

---
+++ src/Unit4Task2.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Unit4Task2 {
+    public static void main(String[] args) {
+        System.out.println("Введите размерность массива: ");
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] mass = new int[n][n];
+
+        for(int i = 0;i < n;i++){
+            for(int j = 0; j < n;j++){
+                mass[i][j] = random.nextInt(9);
+            }
+        }
+        System.out.println("Исходная матрица: ");
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n; j++){
+                System.out.print(mass[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Нечетные элементы находящиеся под главной диагональю");
+        for(int i = 1;i<n;i++){
+            for(int j = 0;j<i;j++){
+                if(mass[i][j]%2!=0) {
+                    System.out.print(mass[i][j] + " ");
+                }
+            }
+        }
+    }
+}
